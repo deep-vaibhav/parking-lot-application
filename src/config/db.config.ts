fa7f5b60092a4841350bfcaf9920a03233359675
Vaibhav Deep <63204318+deep-vaibhav@users.noreply.github.com>
2022-03-15 22:47:02 +0530

    added .env

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -4,7 +4,7 @@ export const connectDB = async () => {
   console.log("⏳ MongoDB Connecting...");
   try {
     await mongoose.connect(
-      "mongodb+srv://parkinglot:parkinglot2022@cluster0.lowch.mongodb.net/parkinglot?retryWrites=true&w=majority"
+      process.env.MONGO_URI
     );
     console.log("✅ MongoDB Connected!");
   } catch (error: any) {
